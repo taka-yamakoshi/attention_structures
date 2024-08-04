1833939a5e094576726d96e993584e14047d7a14
Yamakoshi <tyamakoshi@hawkins-gpu-1.discovery.wisc.edu>
2024-08-04 06:15:07 -0500

    factor out

--- code/dataset.py
+++ code/dataset.py
@@ -119,7 +119,35 @@ class Tree(object):
                 num_tokens += 1
         return nodes, root
 
-    def sample_dfs(self, root: Node, seq_len:int, rng: np.random.Generator):
+    def sample_dfs(self, root: Node, rng: np.random.Generator):
+        stack = [root]
+        nodes = []
+        sent = []
+        while len(stack) > 0:
+            node = stack.pop(-1) # remove the last node in the stack
+            node.id = len(sent) # reset the node id to the position in the sentence
+            nodes.append(node)
+
+            # sample token
+            if len(node.prev)==0:
+                token = rng.choice(self.vocab)
+                token_type = 'a'
+            else:
+                probs = self.transition[node.prev[0].value]
+                token = rng.choice(self.vocab, p=probs)
+                if len(node.next)==0:
+                    node.type = 'c'
+                token_type = node.type
+
+            node.value = token
+            sent.append(token_type+str(token))
+
+            # update stack
+            for child in node.next:
+                stack.append(child)
+        return nodes, sent
+
+    def calc_cross_entropy(self, root: Node, seq_len:int, rng: np.random.Generator, term_prob:float):
         stack = [root]
         nodes = []
         sent = []
@@ -143,18 +171,29 @@ class Tree(object):
 
                 if node.prev[0].type in ['b','c']:
                     # if the preceding token is b or c, then the following token is always a
-                    neg_log_prob = -np.log(probs[token])
+                    assert node.type=='a'
+                    scl = 1.0
                 elif len(node.prev[0].prev)>0:
                     if node.prev[0].prev[0].type in ['b','c']:
                         # if the token second before is b or c, then the following token is either a or c
-                        neg_log_prob = -np.log(probs[token]*0.5)
+                        assert node.type in ['a','c']
+                        scl = term_prob if node.type=='c' else (1-term_prob)
                     else:
-                        neg_log_prob = -np.log(probs[token]*0.3) if node.type=='b' else -np.log(probs[token]*0.35)
+                        if node.type=='b':
+                            scl = 0.3
+                        else:
+                            scl = 0.7*term_prob if node.type=='c' else 0.7*(1-term_prob)
                 elif len(nodes) > seq_len-5:
                     # if the remaining tokens are less than 5, then the following token is either a or c
-                    neg_log_prob = -np.log(probs[token]*0.5)
+                    assert node.type in ['a','c']
+                    scl = term_prob if node.type=='c' else (1-term_prob)
                 else:
-                    neg_log_prob = -np.log(probs[token]*0.3) if node.type=='b' else -np.log(probs[token]*0.35)
+                    if node.type=='b':
+                        scl = 0.3
+                    else:
+                        scl = 0.7*term_prob if node.type=='c' else 0.7*(1-term_prob)
+
+                neg_log_prob = -np.log(probs[token]*scl)
                 cross_entropy += neg_log_prob
             node.value = token
             sent.append(token_type+str(token))
@@ -162,7 +201,7 @@ class Tree(object):
             # update stack
             for child in node.next:
                 stack.append(child)
-        return nodes, sent, cross_entropy
+        return cross_entropy
 
     def convert_to_mat(self, nodes: List[Node]):
         ids = np.array([int(node.__repr__()[1:]) for node in nodes])
@@ -257,12 +296,12 @@ if __name__=='__main__':
         for graph_id, root in enumerate(roots[:100]):
             print(f"Generating sentences for {graph_id}")
             # generate a sentence for calculating the template matrix
-            nodes, _, _ = generator.sample_dfs(root, args.seq_len, rng)
+            nodes, _ = generator.sample_dfs(root, rng)
             mat = generator.convert_to_mat(nodes)
             trn_mats.append(mat)
 
             # generate sentences
-            sents = [' '.join(generator.sample_dfs(root, args.seq_len, rng)[1]) for _ in range(20000)]
+            sents = [' '.join(generator.sample_dfs(root, rng)[1]) for _ in range(20000)]
             sents = list(set(sents))
             assert len(sents) > 10200, "not enough sentences"
 
@@ -300,31 +339,20 @@ if __name__=='__main__':
         # val samples from ood graphs
         ex_val_samples = []
         ex_val_mats = []
-        ex_val_ce = []
         for graph_id, root in enumerate(roots[100:110]):
             # generate a sentence for calculating the template matrix
-            nodes, _, _ = generator.sample_dfs(root, args.seq_len, rng)
+            nodes, _ = generator.sample_dfs(root, rng)
             mat = generator.convert_to_mat(nodes)
             ex_val_mats.append(mat)
 
             # generate sentences
-            sents = []
-            ce = []
-            for _ in range(25):
-                _, sent, cross_entropy = generator.sample_dfs(root, args.seq_len, rng)
-                sent = ' '.join(sent)
-                if sent not in sents:
-                    sents.append(sent)
-                    ce.append(cross_entropy)
-            #sents = list(set(sents))
+            sents = [' '.join(generator.sample_dfs(root, rng)[1]) for _ in range(25)]
+            sents = list(set(sents))
             assert len(sents) > 10, "not enough sentences"
             ex_val_samples.append(sents[:10])
-            ex_val_ce.append(ce[:10])
 
         ex_val_samples = list(np.array(ex_val_samples).T.reshape(10*10))
-        ex_val_ce = list(np.array(ex_val_ce).T.reshape(10*10))
         np.save(f'{args.base_dir}/dataset/{dirname}/ex_val_mat.npy', np.array(ex_val_mats))
-        np.save(f'{args.base_dir}/dataset/{dirname}/ex_val_ce.npy', np.array(ex_val_ce))
         with open(f'{args.base_dir}/dataset/{dirname}/ex_val.txt', 'w') as f:
             f.write('\n'.join(ex_val_samples))
 
@@ -333,12 +361,12 @@ if __name__=='__main__':
         ex_tst_mats = []
         for graph_id, root in enumerate(roots[110:120]):
             # generate a sentence for calculating the template matrix
-            nodes, _, _ = generator.sample_dfs(root, args.seq_len, rng)
+            nodes, _ = generator.sample_dfs(root, rng)
             mat = generator.convert_to_mat(nodes)
             ex_tst_mats.append(mat)
 
             # generate sentences
-            sents = [' '.join(generator.sample_dfs(root, args.seq_len, rng)[1]) for _ in range(25)]
+            sents = [' '.join(generator.sample_dfs(root, rng)[1]) for _ in range(25)]
             sents = list(set(sents))
             assert len(sents) > 10, "not enough sentences"
             ex_tst_samples.append(sents[:10])
