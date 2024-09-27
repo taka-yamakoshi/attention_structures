184726717e8f242cb9c55114594b48dfcc0a1c20
Takateru Yamakoshi <taka@taka-macbook-air.local>
2024-09-27 13:30:53 +0900

    move load_config to utils

--- code/utils.py
+++ code/utils.py
@@ -22,3 +22,54 @@ def gen_run_name_key_amp(args):
     model_stat = f'{args.num_layers}_{args.num_heads}_{args.hidden_size}_{args.intermediate_size}'
     run_stat = f'key_amp_{args.layer_id}_{args.alpha}_{args.datasize}_{args.batchsize_trn}_{args.batchsize_val}_{args.lr}_{args.scheduler_type}_{args.num_epochs}_{args.run_seed}'
     return f'{args.model_type}_{gen_dataset_name(args)}_{model_stat}_{run_stat}'
+
+def load_config(model_type,args):
+    config_kwargs = {
+                    'num_hidden_layers':args.num_layers,
+                    'num_attention_heads':args.num_heads,
+                    'hidden_size':args.hidden_size,
+                    'intermediate_size':args.intermediate_size,
+                    'vocab_size':args.tokenizer.vocab_size,
+                    'max_position_embeddings':32,
+                    'position_embedding_type':'absolute'
+                    }
+    if model_type=='bert':
+        from transformers import BertConfig
+        assert len(args.tokenizer(args.tokenizer.pad_token).input_ids)==3
+        config_kwargs['pad_token_id'] = args.tokenizer(args.tokenizer.pad_token).input_ids[1]
+        return BertConfig(**config_kwargs)
+    elif model_type=='albert':
+        from transformers import AlbertConfig
+        assert len(args.tokenizer(args.tokenizer.pad_token).input_ids)==3
+        assert len(args.tokenizer(args.tokenizer.bos_token).input_ids)==3
+        assert len(args.tokenizer(args.tokenizer.eos_token).input_ids)==3
+        config_kwargs['pad_token_id'] = args.tokenizer(args.tokenizer.pad_token).input_ids[1]
+        config_kwargs['bos_token_id'] = args.tokenizer(args.tokenizer.bos_token).input_ids[1]
+        config_kwargs['eos_token_id'] = args.tokenizer(args.tokenizer.eos_token).input_ids[1]
+        config_kwargs['embedding_size'] = getattr(args,f'hidden_size')
+        return AlbertConfig(**config_kwargs)
+    elif model_type=='gpt2':
+        from transformers import GPT2Config
+        config_kwargs['n_embd'] = config_kwargs.pop('hidden_size')
+        config_kwargs['n_inner'] = config_kwargs.pop('intermediate_size')
+        config_kwargs['n_layer'] = config_kwargs.pop('num_hidden_layers')
+        config_kwargs['n_head'] = config_kwargs.pop('num_attention_heads')
+        config_kwargs['n_positions'] = config_kwargs.pop('max_position_embeddings')
+        _ = config_kwargs.pop('position_embedding_type')
+        assert len(args.tokenizer(args.tokenizer.bos_token).input_ids)==1
+        assert len(args.tokenizer(args.tokenizer.eos_token).input_ids)==1
+        config_kwargs['bos_token_id'] = args.tokenizer(args.tokenizer.bos_token).input_ids[0]
+        config_kwargs['eos_token_id'] = args.tokenizer(args.tokenizer.eos_token).input_ids[0]
+        return GPT2Config(**config_kwargs)
+    elif model_type=='llama2':
+        from transformers import LlamaConfig
+        _ = config_kwargs.pop('position_embedding_type')
+        assert len(args.tokenizer(args.tokenizer.bos_token).input_ids)==2
+        assert len(args.tokenizer(args.tokenizer.eos_token).input_ids)==2
+        assert args.tokenizer(args.tokenizer.bos_token).input_ids[0]==args.tokenizer(args.tokenizer.bos_token).input_ids[1]
+        assert args.tokenizer(args.tokenizer.eos_token).input_ids[0]==args.tokenizer(args.tokenizer.bos_token).input_ids[1]
+        config_kwargs['bos_token_id'] = args.tokenizer(args.tokenizer.bos_token).input_ids[1]
+        config_kwargs['eos_token_id'] = args.tokenizer(args.tokenizer.eos_token).input_ids[1]
+        return LlamaConfig(**config_kwargs)
+    else:
+        raise NotImplementedError
