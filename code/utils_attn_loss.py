b0e99ccc506c3be422634dd992a15d9d1b1b0438
Takateru Yamakoshi <taka@taka-macbook-air.local>
2024-12-10 10:51:21 +0900

    try gpu

--- code/utils_attn_loss.py
+++ code/utils_attn_loss.py
@@ -85,6 +85,7 @@ def create_index_job(xb, args):
         red_dim = 512
     faiss_index = faiss.index_factory(d, f"PCA{red_dim},IVF{args.nlist}_HNSW,Flat")
     #faiss_index = faiss.index_factory(d, f"OPQ16_64,IVF{args.nlist},PQ16x4fsr")
+    faiss_index = faiss.index_cpu_gpu_list(faiss_index, gpus=[1,2,3])
     faiss_index.train(xb)
     faiss_index.add(xb)
     faiss_index.nprobe = args.nprobe
