f19d82a922dccca3e52d24445516af9164095ba0
Taka Yamakoshi <takateru@scdt.stanford.edu>
2025-08-11 02:06:40 -0700

    test

--- code/distill_utils_eval.py
+++ code/distill_utils_eval.py
@@ -151,9 +151,9 @@ def eval_model_mask(tokenizer,model,device,head,text,max_length=None):
             assert " ".join(sent_1.split(" ")[mask_span_start:mask_span_end]).lower()==line[head.index("option_1")].lower()
             assert " ".join(sent_2.split(" ")[mask_span_start:mask_span_end]).lower()==line[head.index("option_2")].lower()
         else:
-            assert " ".join(sent_1.split(" ")[:mask_span_start])==" ".join(sent_2.split(" ")[:mask_span_start])
-            assert " ".join(sent_1.split(" ")[mask_span_end:])==" ".join(sent_2.split(" ")[mask_span_end:])
-            assert " ".join(sent_1.split(" ")[mask_span_start:mask_span_end])!=" ".join(sent_2.split(" ")[mask_span_start:mask_span_end])
+            assert " ".join(sent_1.split(" ")[:mask_span_start])==" ".join(sent_2.split(" ")[:mask_span_start]), f"{sent_1} {sent_2} {mask_span_start} {mask_span_end}"
+            assert " ".join(sent_1.split(" ")[mask_span_end:])==" ".join(sent_2.split(" ")[mask_span_end:]), f"{sent_1} {sent_2} {mask_span_start} {mask_span_end}"
+            assert " ".join(sent_1.split(" ")[mask_span_start:mask_span_end])!=" ".join(sent_2.split(" ")[mask_span_start:mask_span_end]), f"{sent_1} {sent_2} {mask_span_start} {mask_span_end}"
         logprob_1 = calc_prob_mask(tokenizer, model, device, sent_1, [mask_span_start,mask_span_end])
         logprob_2 = calc_prob_mask(tokenizer, model, device, sent_2, [mask_span_start,mask_span_end])
         new_text.append(line+[logprob_1,logprob_2])
